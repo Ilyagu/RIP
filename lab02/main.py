8b0e5c6f49f480dad04b0048bda0cf88012fa4a2
Ilyagu <inagdimaev@mail.ru>
2020-09-23 02:15:20 +0300

    lab02

--- lab02/main.py
+++ lab02/main.py
@@ -1,14 +1,17 @@
 from lab_python_oop.Rectangle import Rectangle
 from lab_python_oop.Circle import Circle
 from lab_python_oop.Square import Square
-
+from colorama import Back,Fore,Style
 
 def main():
     r = Rectangle(12, 12,"синего")
     c = Circle(12,"зеленого")
     s = Square(12,"красного")
+    print(Back.BLUE)
     print(r)
+    print(Back.GREEN)
     print(c)
+    print(Back.RED)
     print(s)
 
 if __name__ == "__main__":
